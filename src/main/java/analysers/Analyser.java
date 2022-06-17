2391971adca4f13b3d13f9eb7672faf7b4b7892a
Kirill <kirya.girman@mail.ru>
2022-06-17 11:53:25 +0300

    Realised Analyser's and Language's methods

--- src/main/java/analysers/Analyser.java
+++ src/main/java/analysers/Analyser.java
@@ -30,6 +30,20 @@ public class Analyser{
         return frequencies;
     }
 
+    public void addLanguage(Language language){
+        languages.add(language);
+    }
+
+    public void generateFrequencies(Reader reader){
+        generateText(reader);
+        analyse();
+    }
+
+    public void generateText(Reader reader){
+        // copy text from Reader where it is stored
+        text.addAll(reader.getText());
+    }
+
     // PRINT
 
     public void printTextBySymbols(){
@@ -50,6 +64,118 @@ public class Analyser{
 
     public void printRemovedStopSymbols(){
         removedStopSymbols.forEach(System.out::print);
+    }
+
+    // SORT FREQUENCIES
+
+    public void sortFrequenciesByKey(){
+        putSortedEntries(getSortedEntries(true));
+    }
+
+    public void sortFrequenciesByValue(){
+        putSortedEntries(getSortedEntries(false));
+    }
+
+    private ArrayList<Entry<String, Integer>> getSortedEntries(boolean sortByKey){
+        // there are different algorithms depending on sorting by key or by value
+        if(!sortByKey) sortFrequenciesByKey();
+        // create ArrayList of frequencies entries
+        ArrayList<Entry<String, Integer>> entries = new ArrayList<>(frequencies.entrySet());
+        // sort entries by key or by value
+        Collections.sort(entries, (Entry<String, Integer> a, Entry<String, Integer> b) ->{
+            if(sortByKey) return a.getKey().compareTo(b.getKey());
+            else return b.getValue().compareTo(a.getValue());
+        });
+        // return sorted entries
+        return entries;
+    }
+
+    private void putSortedEntries(ArrayList<Entry<String, Integer>> entries){
+        // delete all entries
+        frequencies.clear();
+        // fill frequencies with sorted entries
+        for (Entry<String, Integer> entry : entries){
+            frequencies.put(entry.getKey(), entry.getValue());
+        }
+    }
+
+    // ANALYSE TEXT
+
+    public void analyse(){ // this method calculates frequency of each symbol
+        // delete previous data about frequencies
+        frequencies.clear();
+        for(String symbol : text){
+            // convert each stop symbol to lower case
+            symbol = symbol.toLowerCase();
+            // check if this symbol is stop or inviolable symbol
+            boolean inviolableSymbol = symbol.charAt(0) == '\r' || symbol.charAt(0) == '\n' || symbol.charAt(0) == ' ';
+            boolean stopSymbol = getStopSymbols().contains(symbol);
+            if(inviolableSymbol || stopSymbol) continue;
+            // search for this symbol in HashMap with frequencies
+            Integer symbolFrequency;
+            try{
+                // if key "symbol" exists, increment its value
+                symbolFrequency = frequencies.get(symbol);
+                if(symbolFrequency == null) throw new NullPointerException("No such symbol in HashMap frequencies");
+            } catch(NullPointerException e){
+                // initialize start value
+                symbolFrequency = 0;
+            }
+            // add new value of this symbol frequency
+            frequencies.put(symbol, symbolFrequency+1);
+        }
+    }
+
+    // CLEAR TEXT
+
+    public void clearText(HashSet<String> stopSymbols){
+        for(int i=text.size()-1; i>=0; i--){
+            // get another symbol in the text
+            String symbolInText = text.get(i).toLowerCase();
+            // check if this symbol equals a banned one
+            for(String symbol : stopSymbols){
+                if(symbolInText.equals(symbol)){
+                    // if it does, delete it from the text
+                    removedStopSymbols.add(symbol);
+                    text.remove(i);
+                    break;
+                }
+            }
+        }
+    }
+
+    public void clearText(){ // remove each stop word from the text
+        clearText(getStopSymbols());
+    }
+
+    // STOP SYMBOLS
+
+    private HashSet<String> getStopSymbols(){
+        HashSet<String> stopSymbolsFromEachLanguage = new HashSet<>();
+        // this set will contain each stop symbol in each language
+        for(Language language : languages){
+            // check each existing language
+            // each stop symbol is already in lower case
+            var stopSymbols = language.getStopSymbols();
+            for(String stopSymbol : stopSymbols.keySet()){
+                // check each stop symbol in the language
+                if(stopSymbols.get(stopSymbol)){
+                    // if this stop symbol is ABLE, add it
+                    stopSymbolsFromEachLanguage.add(stopSymbol);
+                }
+            }
+        }
+        return stopSymbolsFromEachLanguage;
+    }
+
+    // CASE
+
+    public void convertToCase(boolean toUpperCase){
+        for(int i=0; i<text.size(); i++){
+            String symbol = text.get(i);
+            // change each symbol's case to upper or lower
+            text.set(i, toUpperCase ? symbol.toUpperCase() : symbol.toLowerCase());
+        }
     }
 
 }
--- src/main/java/languages/Language.java
+++ src/main/java/languages/Language.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Language{
-    // Language may be RU, EN, punctuation marks
+    // Language may be RU, EN, punctuation marks (PM)
 
     private String code;
     private HashMap<String, Boolean> stopSymbols; // all stop symbols are in lower case
@@ -22,6 +22,39 @@ public class Language{
 
     public HashMap<String, Boolean> getStopSymbols() {
         return stopSymbols;
+    }
+
+    public void generateStopSymbols(Reader reader){
+        // copy each symbol from Reader where they are stored
+        for(String stopSymbol : reader.getText()){
+            // check if symbol is \r, \n or space
+            char stopSymbolChar = stopSymbol.charAt(0);
+            boolean inviolableSymbol = stopSymbolChar == '\r' || stopSymbolChar == '\n' || stopSymbolChar == ' ';
+            // if it is, do not add them to HashMap
+            if(!inviolableSymbol) addStopSymbol(stopSymbol);
+        }
+    }
+
+    public void makeDisable(String symbol){
+        if(!keyExists(symbol)) throw new RuntimeException("There is no such symbol as "+symbol);
+        getStopSymbols().put(symbol.toLowerCase(), false);
+    }
+
+    public void makeAble(String symbol){
+        if(!keyExists(symbol)) throw new RuntimeException("There is no such symbol as "+symbol);
+        getStopSymbols().put(symbol.toLowerCase(), true);
+    }
+
+    public boolean keyExists(String symbol){
+        return getStopSymbols().containsKey(symbol.toLowerCase());
+    }
+
+    public void addStopSymbol(String stopSymbol){
+        getStopSymbols().put(stopSymbol.toLowerCase(), true);
+    }
+
+    public void printStopSymbols(){
+        stopSymbols.forEach((key, value) -> System.out.println("Symbol: \"" + key + "\" -> " + value));
     }
 
 }
