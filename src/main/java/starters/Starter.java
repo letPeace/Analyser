c4f29b24ccaff4dd840f0a33b7af1774c327af3f
Kirill <kirya.girman@mail.ru>
2022-06-18 02:00:09 +0300

    Added JAR for the first release.

--- src/main/java/starters/Starter.java
+++ src/main/java/starters/Starter.java
@@ -10,29 +10,6 @@ public class Starter{
 
     public static void main(String[] args){
         Interface app = new Interface();
-        //
-        /*Reader stopRU = new Reader("D:\\_Mehi\\6sem\\java\\analyser\\src\\main\\resources\\stopRU.txt");
-        stopRU.read();
-        Language russian = new Language("ru");
-        russian.generateStopSymbols(stopRU);
-        //
-        Reader stopPM = new Reader("D:\\_Mehi\\6sem\\java\\analyser\\src\\main\\resources\\stopPM.txt");
-        stopPM.read();
-        Language punctuationMark = new Language("pm");
-        punctuationMark.generateStopSymbols(stopPM);
-        //
-        Reader textReader = new Reader("D:\\_Mehi\\6sem\\java\\analyser\\src\\main\\resources\\text.txt");
-        textReader.read();
-        //
-        Analyser a = new Analyser();
-        a.generateText(textReader);
-        a.analyse();
-        a.sortFrequenciesByValue();
-        //
-        Writer textWriter = new Writer("D:\\_Mehi\\6sem\\java\\analyser\\src\\main\\resources\\out\\text.txt", a);
-        textWriter.saveText();
-        Writer frequenciesWriter = new Writer("D:\\_Mehi\\6sem\\java\\analyser\\src\\main\\resources\\out\\frequencies.xlsx", a);
-        frequenciesWriter.saveFrequencies();*/
     }
 
 }
